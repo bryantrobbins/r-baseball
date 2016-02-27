89750cfd5e004fed2f7f8cb1aa04bc7764101b9c
Bryan Robbins <bryan.robbins@finra.org>
2016-02-27 00:06:41 -0500

    Add groovy download to build process

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 
 ./clean.sh
 
+echo "Download groovy (temp)"
+wget -O groovy.jar http://search.maven.org/remotecontent?filepath=org/codehaus/groovy/groovy/2.4.6/groovy-all-2.4.6.jar
+
 echo "Download and transform raw data"
 pushd extract
-groovy download.groovy
+java -jar ../groovy.jar download.groovy
 popd
 
 echo "Create redistributable package"
